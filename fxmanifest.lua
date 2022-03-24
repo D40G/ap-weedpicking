267c9c8ec7b90ace578b2c8c9477be0a579fec21
AppleSatin <99192090+AppleSatin@users.noreply.github.com>
2022-03-24 13:59:36 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'QB-WeedPicking'
+
+server_scripts {
+	'server/weedplant.lua'
+}
+
+client_scripts {
+	'client/weedplant.lua'
+}
+
+lua54 'yes'
---
+++ shared.lua
@@ -0,0 +1,4 @@
+-- Add this in Shared.lua of qb-core/shard.lua
+
+['weed_wet'] 				  	 = {['name'] = 'weed_wet', 			 	    	['label'] = 'Wet Weed', 				['weight'] = 2000, 		['type'] = 'item', 		['image'] = 'wet.png', 		        	['unique'] = false, 	['useable'] = false, 	['shouldClose'] = false,   ['combinable'] = nil,   ['description'] = 'A Stick Of Wet Weed..'},
+['weed_bud'] 				  	 = {['name'] = 'weed_bud', 			     		['label'] = 'A Gram Of Bud', 			['weight'] = 1000, 		['type'] = 'item', 		['image'] = 'buddry2.png',  			['unique'] = false, 	['useable'] = true, 	['shouldClose'] = false,   ['combinable'] = nil,   ['description'] = 'A Small Bud..'},
